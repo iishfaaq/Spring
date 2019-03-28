20403e9f54d18c33d84087b4fcf288acbf769e5b
iishfaaq <36529006+iishfaaq@users.noreply.github.com>
2019-03-28 17:05:43 +0530

    Student service after adding ApplicationContext

--- sms/src/com/ishfaaq/sms/service/StudentServiceimpl.java
+++ sms/src/com/ishfaaq/sms/service/StudentServiceimpl.java
@@ -6,12 +6,23 @@ import com.ishfaaq.sms.model.Student;
 import com.ishfaaq.sms.repository.HibernateStudentRepositoryimpl;
 import com.ishfaaq.sms.repository.StudentRepository;
 
+
 public class StudentServiceimpl implements StudentService {
 	
 	// tight coupling 
-	StudentRepository repository = new HibernateStudentRepositoryimpl();
+	StudentRepository repository; //= new HibernateStudentRepositoryimpl();
 	
 	
+	public StudentRepository getRepository() {
+		return repository;
+	}
+
+
+	public void setRepository(StudentRepository repository) {
+		this.repository = repository;
+	}
+
+
 	public List<Student> fetchAllStudents(){
 		return repository.fetchAllStudents();
 	}
