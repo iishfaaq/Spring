9b0aa02b07e7317679191618145b599a2e5f2435
iishfaaq <36529006+iishfaaq@users.noreply.github.com>
2019-04-01 14:18:52 +0530

    added Sout in dflt constrctr

--- sms/src/com/ishfaaq/sms/service/StudentServiceimpl.java
+++ sms/src/com/ishfaaq/sms/service/StudentServiceimpl.java
@@ -1,40 +1,40 @@
-package com.ishfaaq.sms.service;
-
-import java.util.List;
-
-import com.ishfaaq.sms.model.Student;
-import com.ishfaaq.sms.repository.HibernateStudentRepositoryimpl;
-import com.ishfaaq.sms.repository.StudentRepository;
-
-
-public class StudentServiceimpl implements StudentService {
+	package com.ishfaaq.sms.service;
 	
-	// tight coupling 
-	StudentRepository studentrepository; //= new HibernateStudentRepositoryimpl();
+	import java.util.List;
 	
-	public StudentServiceimpl() {
+	import com.ishfaaq.sms.model.Student;
+	import com.ishfaaq.sms.repository.HibernateStudentRepositoryimpl;
+	import com.ishfaaq.sms.repository.StudentRepository;
+	
+	
+	public class StudentServiceimpl implements StudentService {
 		
-	}
+		// tight coupling 
+		StudentRepository studentrepository; //= new HibernateStudentRepositoryimpl();
+		
+		public StudentServiceimpl() {
+			System.out.println("Default constructer fired");
+		}
+		
+		public StudentServiceimpl(StudentRepository repository) {
+			System.out.println("constructoer fired");
+			this.studentrepository = repository;
+		}
+		
+		
+		public StudentRepository getStudentRepository() {
+			return studentrepository;
+		}
 	
-	public StudentServiceimpl(StudentRepository repository) {
-		System.out.println("constructoer fired");
-		this.studentrepository = repository;
-	}
+	
+		public void setStudentRepository(StudentRepository repository) {
+			System.out.println("Setter Fiered");
+			this.studentrepository = repository;
+		}
 	
 	
-	public StudentRepository getStudentRepository() {
-		return studentrepository;
+		public List<Student> fetchAllStudents(){
+			return studentrepository.fetchAllStudents();
+		}
+	
 	}
-
-
-	public void setStudentRepository(StudentRepository repository) {
-		System.out.println("Setter Fiered");
-		this.studentrepository = repository;
-	}
-
-
-	public List<Student> fetchAllStudents(){
-		return studentrepository.fetchAllStudents();
-	}
-
-}
